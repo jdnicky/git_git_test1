a134cb64747e340912683b7b6d2d9a30edf3200a
nicky <51666293@qq.com>
2022-05-27 11:11:10 +0800

    first commit

--- src/main/java/org/example/GitTest3.java
+++ src/main/java/org/example/GitTest3.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.omg.CORBA.SetOverrideType;
+
 public class GitTest3 {
     public static void main(String[] args) {
         System.out.println("aaaaaa");
-
+        System.out.println("master");
     }
 }
