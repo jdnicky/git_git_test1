1f2345621fd2dd5621ec08e6e8ef628cb5f76575
nicky <51666293@qq.com>
2022-05-27 11:09:53 +0800

    first commit

--- src/main/java/org/example/GitTest3.java
+++ src/main/java/org/example/GitTest3.java
@@ -3,6 +3,6 @@ package org.example;
 public class GitTest3 {
     public static void main(String[] args) {
         System.out.println("aaaaaa");
-
+        System.out.println("first_branch");
     }
 }
